ddc102b481c0e2088db56b4ccdb2b4b02c7b8540
Aru Sahni <aru@arusahni.net>
2026-07-25 11:03:36 -0400

    editor: improve code action placement for soft-wrapped display rows
    Because soft-wrapped display rows now can start on a *lower* indent
    column than the first display row, we cannot rely on the first row's
    indent for code action placement, and instead need to check to see if
    there's enough space to display the code action glyph. If not, use the
    buffer's first display row for the calculating code action placement.
    
    (cherry picked from commit 6d5e00e2f9dbc43b13ba6c7d1de95ffde1a7cdeb)

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -11656,7 +11656,23 @@ impl EditorSnapshot {
                     .display_snapshot
                     .buffer_snapshot()
                     .indent_size_for_line(MultiBufferRow(row_candidate));
-                indent_size.len >= INLINE_SLOT_CHAR_LIMIT
+                if indent_size.len >= INLINE_SLOT_CHAR_LIMIT {
+                    true
+                } else if row_candidate == buffer_point.row {
+                    let display_row = self
+                        .display_snapshot
+                        .point_to_display_point(buffer_point, text::Bias::Left)
+                        .row();
+                    if display_row.0 > 0 {
+                        self.display_snapshot
+                            .soft_wrap_indent(DisplayRow(display_row.0 - 1))
+                            .is_some_and(|indent| indent >= INLINE_SLOT_CHAR_LIMIT)
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
             }
         };
 
@@ -11677,7 +11693,7 @@ impl EditorSnapshot {
             })
         }?;
 
-        let new_display_row = self
+        let mut new_display_row = self
             .display_snapshot
             .point_to_display_point(
                 Point {
@@ -11687,6 +11703,24 @@ impl EditorSnapshot {
                 text::Bias::Left,
             )
             .row();
+
+        if new_display_row.0 > 0
+            && self
+                .display_snapshot
+                .soft_wrap_indent(DisplayRow(new_display_row.0 - 1))
+                .is_some_and(|indent| indent < INLINE_SLOT_CHAR_LIMIT)
+        {
+            new_display_row = self
+                .display_snapshot
+                .point_to_display_point(
+                    Point {
+                        row: new_buffer_row,
+                        column: 0,
+                    },
+                    text::Bias::Left,
+                )
+                .row();
+        }
 
         Some(new_display_row)
     }
